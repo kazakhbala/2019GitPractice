c308802cb6d982fb071fc7188804f99de7d1844f
Aibek-KZ <55055591+Aibek-KZ@users.noreply.github.com>
2019-11-22 14:19:17 -0700

    Update Cars.java
    Add some Selenium stuff

--- src/main/java/java_code/Cars.java
+++ src/main/java/java_code/Cars.java
@@ -3,7 +3,12 @@ package java_code;
 public class Cars {
     private String name;
     public int numbers;
+    String human;
+    
+    
     public static void main(String[] args) {
         System.out.println("I love Java");
+        System.out.println("I love Java an d Selenium");
+
     }
 }
