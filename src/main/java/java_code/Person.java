293a955de4294cc24cd1aa183387b49de42e6178
Aibek-KZ <aibek.risbek@gmail.com>
2019-11-22 13:44:07 -0700

    second time  trying

--- src/main/java/java_code/Person.java
+++ src/main/java/java_code/Person.java
@@ -7,6 +7,7 @@ public class Person {
     public static void main(String[] args) {
         System.out.println("My code");
         System.out.println("I got an offer letter today");
+        System.out.println("Happy");
     }
 
 
