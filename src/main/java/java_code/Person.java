1ae0d2462e77ebe73dea6abd76f28694c7e87192
Aibek-KZ <55055591+Aibek-KZ@users.noreply.github.com>
2019-11-22 13:54:26 -0700

    Update Person.java
    Sorry I did

--- src/main/java/java_code/Person.java
+++ src/main/java/java_code/Person.java
@@ -4,17 +4,13 @@ public class Person {
     int age;
 <<<<<<< HEAD
     String name;
-    String money;
+    
 
     public static void main(String[] args) {
-        System.out.println("M y code ");
+       
         System.out.println("I  got an offer letter today ");
-        System.out.println(" Happy ");
+        System.out.println("I am so Happy ");
     }
-
-=======
     
-    private String car;
->>>>>>> 7d791394cefef0ef4bbeb6106a6fb02a077eabaf
-
+   
 }
