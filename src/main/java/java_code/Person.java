9a07061cf85829e25edc5183958e4454beb23bc6
Aibek-KZ <aibek.risbek@gmail.com>
2019-11-22 13:46:57 -0700

    third commit and try

--- src/main/java/java_code/Person.java
+++ src/main/java/java_code/Person.java
@@ -3,11 +3,12 @@ package java_code;
 public class Person {
     int age;
     String name;
+    String money;
 
     public static void main(String[] args) {
-        System.out.println("My code");
-        System.out.println("I got an offer letter today");
-        System.out.println("Happy");
+        System.out.println("M y code ");
+        System.out.println("I  got an offer letter today ");
+        System.out.println(" Happy ");
     }
 
 
