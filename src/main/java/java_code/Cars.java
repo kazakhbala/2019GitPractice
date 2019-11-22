2a315f90cb4b012c52afa1d52c03d479cb4a88a9
Aibek-KZ <aibek.risbek@gmail.com>
2019-11-22 14:16:42 -0700

    Created Cars file

---
+++ src/main/java/java_code/Cars.java
@@ -0,0 +1,9 @@
+package java_code;
+
+public class Cars {
+    private String name;
+    public int numbers;
+    public static void main(String[] args) {
+        System.out.println("I love Java");
+    }
+}
