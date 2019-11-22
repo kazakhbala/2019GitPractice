b686a8db479a7e7c84a5733d8d1feea973deced5
Aibek-KZ <aibek.risbek@gmail.com>
2019-11-22 13:57:25 -0700

    add some inter

--- src/main/java/java_code/Person.java
+++ src/main/java/java_code/Person.java
@@ -2,14 +2,18 @@ package java_code;
 
 public class Person {
     int age;
-<<<<<<< HEAD
     String name;
+
     
 
     public static void main(String[] args) {
        
         System.out.println("I  got an offer letter today ");
         System.out.println("I am so Happy ");
+        System.out.println("Finally I have  learned GitHub");
+        System.out.println("I am so reach");
+
+
     }
     
    
