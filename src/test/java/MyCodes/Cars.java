7d02ea7fd9b83fb01c551e34ed072cbeb2be80ae
Aibek-KZ <aibek.risbek@gmail.com>
2020-02-15 15:17:13 -0700

    added susburu

--- src/test/java/MyCodes/Cars.java
+++ src/test/java/MyCodes/Cars.java
@@ -14,6 +14,7 @@ public class Cars {
         System.out.println("Tesla");
         System.out.println("Honda");
         System.out.println("Lada");
+        System.out.println("Suburu");
 
     }
 }
