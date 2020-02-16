2787c10332946b265de620e85aca559cb12ec2f9
Aibek-KZ <aibek.risbek@gmail.com>
2020-02-15 22:42:35 -0700

    emty

--- src/test/java/MyCodes/Cars.java
+++ src/test/java/MyCodes/Cars.java
@@ -8,14 +8,7 @@ public class Cars {
     
     public static void main(String[] args) {
         System.out.println("I love Java");
-        System.out.println("I love Java an d Selenium");
-        System.out.println("Lexus570");
-        System.out.println("BMW No Mercedes");
-        System.out.println("Tesla");
-        System.out.println("Honda");
-        System.out.println("Lada");
-        System.out.println("Suburu");
-        System.out.println("Ram");
+
 
     }
 }
